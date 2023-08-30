571ed91e4172619522247939282400ba6eb0ef99
valerioprds <v.paradiso89@gmail.com>
2023-08-30 11:40:24 +0200

    some fixes

--- src/app/components/email-verification/email-verification.component.ts
+++ src/app/components/email-verification/email-verification.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-email-verification',
   templateUrl: './email-verification.component.html',
-  styleUrls: ['./email-verification.component.css']
+  styleUrls: ['./email-verification.component.css'],
 })
 export class EmailVerificationComponent {
-
+  @Input() userDetails: any;
 }
--- src/app/utils/toiletLocation.interface.ts
+++
@@ -1,7 +0,0 @@
-// location.model.ts
-export interface ToiletLocation {
-  _id: string;
-  longitude: number;
-  latitude: number;
-  name: string;
-}
