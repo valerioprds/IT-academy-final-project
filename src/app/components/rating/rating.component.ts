31180e989fdb0750d5816ea2ce5583b5554ee33b
valerioprds <v.paradiso89@gmail.com>
2023-09-25 16:10:06 +0200

    added average rating

--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MapService } from 'src/app/services/map.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-rating',
@@ -11,18 +12,29 @@ import { MapService } from 'src/app/services/map.service';
 export class RatingComponent implements OnInit {
   @Input() toiletId: string;
 
-  constructor(private http: HttpClient) {}
-  ngOnInit(): void {
-    console.log('Received toiletId:', this.toiletId);
-  }
+  // Variables to keep track of the total sum and count of ratings
+  totalRatingSum: number = 0;
+  totalRatingCount: number = 0;
+  averageRating: number = 0;
+
+  constructor(private http: HttpClient, private toastr: ToastrService) {}
+
+  ngOnInit(): void {}
 
   postRating(toiletId: string, userRating: number) {
+    // Update the total sum and count of ratings
+    this.totalRatingSum += userRating;
+    this.totalRatingCount++;
+
+    // Calculate the average rating
+    this.averageRating = this.totalRatingSum / this.totalRatingCount;
+
     const payload = {
       toiletId,
       userRating,
     };
 
-    console.log(payload); //toiletId is undefined
+    console.log(payload);
 
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
@@ -34,13 +46,12 @@ export class RatingComponent implements OnInit {
       })
       .subscribe(
         (res) => {
-          console.log('Rating posted successfully:', res);
+          this.toastr.success('Rating posted successfully:');
+          console.log('Average Rating:', this.averageRating); // Log the average rating
         },
         (err) => {
-          console.error('Failed to post rating:', err);
+          this.toastr.error('Failed to post rating:', err);
         }
       );
   }
-
- 
 }
