8abd36a7e22ac439f427d8c1f39a27340e8db503
valerioprds <v.paradiso89@gmail.com>
2023-09-26 16:54:50 +0200

    each toilet with different average

--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -21,7 +21,48 @@ export class RatingComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  // Use a map to maintain state for each toilet
+  toiletRatingsMap: Map<
+    string,
+    { totalRatingSum: number; totalRatingCount: number; averageRating: number }
+  > = new Map();
+
   postRating(toiletId: string, userRating: number) {
+    let toiletRating = this.toiletRatingsMap.get(toiletId);
+    if (!toiletRating) {
+      toiletRating = {
+        totalRatingSum: 0,
+        totalRatingCount: 0,
+        averageRating: 0,
+      };
+      this.toiletRatingsMap.set(toiletId, toiletRating);
+    }
+
+    // Update the total sum and count of ratings for the specific toilet
+    toiletRating.totalRatingSum += userRating;
+    toiletRating.totalRatingCount++;
+
+    // Calculate the average rating for the specific toilet
+    toiletRating.averageRating =
+      toiletRating.totalRatingSum / toiletRating.totalRatingCount;
+
+    // ... rest of your code
+    console.log(
+      'Average Rating for toilet',
+      toiletId,
+      ':',
+      toiletRating.averageRating
+    );
+  }
+
+
+  getAverageRating(toiletId: string): number {
+    const toiletRating = this.toiletRatingsMap.get(toiletId);
+    return toiletRating ? toiletRating.averageRating : 0;
+  }
+
+
+  /* postRating(toiletId: string, userRating: number) {
     // Update the total sum and count of ratings
     this.totalRatingSum += userRating;
     this.totalRatingCount++;
@@ -53,5 +94,5 @@ export class RatingComponent implements OnInit {
           this.toastr.error('Failed to post rating:', err);
         }
       );
-  }
+  } */
 }
