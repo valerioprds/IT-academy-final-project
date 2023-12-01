abeb23a85055e24520ae59c090ace15ac74c09dd
valerioprds <v.paradiso89@gmail.com>
2023-12-01 13:06:56 +0100

    fixed login styles

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -4,19 +4,79 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { FirebaseCodeErrorService } from 'src/app/services/firebase-code-error.service';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
+  animations: [
+    trigger('slideIn', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)', opacity: 0 }),
+        animate(
+          '1s ease-out',
+          style({ transform: 'translateX(0)', opacity: 1 })
+        ),
+      ]),
+    ]),
+    trigger('focusPanel', [
+      state(
+        'inactive',
+        style({
+          transform: 'scale(1)',
+        })
+      ),
+      state(
+        'active',
+        style({
+          transform: 'scale(1.05)',
+        })
+      ),
+      transition('inactive => active', animate('500ms ease-in')),
+      transition('active => inactive', animate('500ms ease-out')),
+    ]),
+    trigger('flipIcon', [
+      state(
+        'default',
+        style({
+          transform: 'rotate(0)',
+        })
+      ),
+      state(
+        'flipped',
+        style({
+          transform: 'rotate(180deg)',
+        })
+      ),
+      transition('default <=> flipped', animate('300ms')),
+    ]),
+  ],
 })
 export class LoginComponent implements OnInit {
+  // animations
+  focusState: string = 'inactive';
+  iconState: string = 'default';
+
+  toggleFocusState() {
+    this.focusState = this.focusState === 'inactive' ? 'active' : 'inactive';
+  }
+  toggleIconState() {
+    this.iconState = this.iconState === 'default' ? 'flipped' : 'default';
+  }
+
   loginForm: FormGroup;
   loading: boolean = false;
 
   // to visualize the password
-  visible:boolean = true;
-  changetype:boolean =true;
+  visible: boolean = true;
+  changetype: boolean = true;
 
   constructor(
     private fb: FormBuilder,
@@ -33,8 +93,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {}
 
-
-  viewpass(){
+  viewpass() {
     this.visible = !this.visible;
     this.changetype = !this.changetype;
   }
@@ -48,7 +107,7 @@ export class LoginComponent implements OnInit {
     this.afAuth
       .signInWithEmailAndPassword(email, password)
       .then((user) => {
-       // console.log(user);
+        // console.log(user);
         if (user.user?.emailVerified) {
           this.router.navigate(['/dashboard']);
         } else {
@@ -57,7 +116,7 @@ export class LoginComponent implements OnInit {
       })
       .catch((error) => {
         this.loading = false;
-       // console.log(error);
+        // console.log(error);
         this.toastr.error(this.firebaseError.codeError(error.code), 'Error');
       });
   }
